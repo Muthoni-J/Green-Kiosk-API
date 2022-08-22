4c439c7bb9c3661a5fe8425fdbd5340f5b903ac0
Muthoni-J <jmjoanmuthoni@gmail.com>
2022-08-22 08:21:43 +0300

    changes

--- Rest.js
+++ Rest.js
@@ -4,7 +4,6 @@ let vegetables = document.getElementById("vegList")
 document.getElementById("title").style.color="green";
 document.body.style.backgroundColor = "silver";
 
-
 const baseUrl='http://localhost:5000';
 
 fetch(`${baseUrl}/products/fruits`)
@@ -53,32 +52,3 @@ fetch(`${baseUrl}/products/vegetables`)
     console.log(error)
 })
 
-
-// const { METHODS } = require("http")
-// const { title } = require("process")
-
-// let productslist=document.getElementById("product")
-// productslist.firstElementChild('/products',{
-//     method:'GET',
-//     headers:{
-//         "Content-type":"application/json"
-//     }
-// })
-
-
-// let veglist=document.getElementById("vegetables")
-// veglist.firstElementChild('/vegetables',{
-//     method:'GET',
-//     headers:{
-//         "Content-type":"application/json"
-//     }
-// })
-
-
-// let frulist=document.getElementById("fruits")
-// frulist.firstElementChild('/fruits',{
-//     method:'GET',
-//     headers:{
-//         "Content-type":"application/json"
-//     }
-// })
